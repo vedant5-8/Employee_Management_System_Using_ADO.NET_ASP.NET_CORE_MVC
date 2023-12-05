6a873cbd06ab590519820e00f5f0b3c1206cbaff
Vedant-Patil <vedantnp200@outlook.com>
2023-12-06 00:14:10 +0530

    Employee Management System using ADO.NET project is created.

---
+++ Controllers/EmployeeManagementController.cs
@@ -0,0 +1,82 @@
+﻿using Employee_Mnagement_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace Employee_Mnagement_System.Controllers
+{
+    public class EmployeeManagementController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private string connectionString;
+
+        public EmployeeManagementController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IActionResult Index()
+        {
+            List<EmployeeModel> employeeList = new List<EmployeeModel>();
+            const string Query = "select * from employee;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(Query, connection))
+                {
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        EmployeeModel employee = new EmployeeModel();
+
+                        employee.Id = (int) reader["Id"];
+                        employee.FirstName = reader["FirstName"].ToString();
+                        employee.LastName = reader["LastName"].ToString();
+                        employee.EmailId = reader["EmailId"].ToString();
+                        employee.ContactNo = reader["ContactNo"].ToString();
+                        employee.Age = (int) reader["Age"];
+
+                        employeeList.Add(employee);
+                    }
+                }
+            }
+
+            return View(employeeList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeModel employee)
+        {
+
+            const string Query = "Insert into employee (FirstName, LastName, EmailId, ContactNo, Age) values (@FirstName, @LastName, @EmailId, @ContactNo, @Age);";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(Query, connection))
+                {
+                    connection.Open();
+
+                    command.Parameters.AddWithValue("@FirstName", employee.FirstName);
+                    command.Parameters.AddWithValue("@LastName", employee.LastName);
+                    command.Parameters.AddWithValue("@EmailId", employee.EmailId);
+                    command.Parameters.AddWithValue("@ContactNo", employee.ContactNo);
+                    command.Parameters.AddWithValue("@Age", employee.Age);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Mnagement_System.Models
+{
+    public class EmployeeModel
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailId { get; set; }
+
+        public string ContactNo { get; set; }
+
+        public int Age { get; set; }
+    }
+}
